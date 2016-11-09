4de543c09d4053ccfe653c756e692d5c824e1ab8
ricaportela <ricaportela@gmail.com>
2016-11-09 06:28:30 -0200

    First Commit

---
+++ employees/core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from employees.core.models import Departament, Employees
+
+
+@admin.register(Departament)
+class DepartamentAdmin(admin.ModelAdmin):
+    list_display = ('description')
+
+
+@admin.register(Employees)
+class EmployeesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'departament')
+
---
+++ employees/core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Employees
+from .serializers import EmployeesSerializer
+
+
+class EmployeesList(APIView):
+    """
+    Returns all employees in the database
+    """
+    def get(self, request):
+        employees = Employees.objects.all()
+        serializer = EmployeesSerializer(employees, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):    
+        raise PermissionDenied
